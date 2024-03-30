168f8a129df5cd237236315b9e66417f7c36668c
tmaxell <tmaxelhard@gmail.com>
2024-03-30 21:21:55 +0300

    fixing problems with pheromone function

--- main.py
+++ main.py
@@ -5,65 +5,6 @@ import math
 from itertools import permutations
 import random
 
-class AntColony:
-    def __init__(self, graph, ants_count=10, evaporation_rate=0.1, pheromone_deposit=1, alpha=1, beta=2, iterations=100):
-        self.graph = graph
-        self.ants_count = ants_count
-        self.evaporation_rate = evaporation_rate
-        self.pheromone_deposit = pheromone_deposit
-        self.alpha = alpha
-        self.beta = beta
-        self.iterations = iterations
-
-    def find_shortest_path(self):
-        shortest_path = None
-        min_cycle_length = float('inf')
-
-        for _ in range(self.iterations):
-            paths = self.generate_ant_paths()
-            self.update_pheromones(paths)
-            current_shortest_path, current_cycle_length = self.get_shortest_path(paths)
-            if current_cycle_length < min_cycle_length:
-                shortest_path = current_shortest_path
-                min_cycle_length = current_cycle_length
-
-        return shortest_path, min_cycle_length
-
-    def generate_ant_paths(self):
-        paths = []
-        for _ in range(self.ants_count):
-            path = self.generate_ant_path()
-            paths.append(path)
-        return paths
-
-    def generate_ant_path(self):
-        nodes = list(self.graph.nodes)
-        random.shuffle(nodes)
-        return nodes
-
-    def update_pheromones(self, paths):
-        for path in paths:
-            cycle_length = sum(self.graph[path[i]][path[i+1]]['weight'] for i in range(len(path) - 1))
-            cycle_length += self.graph[path[-1]][path[0]]['weight']
-            for i in range(len(path) - 1):
-                self.graph[path[i]][path[i+1]]['pheromone'] += self.pheromone_deposit / cycle_length
-            self.graph[path[-1]][path[0]]['pheromone'] += self.pheromone_deposit / cycle_length
-
-        for edge in self.graph.edges:
-            self.graph.edges[edge]['pheromone'] *= (1 - self.evaporation_rate)
-
-    def get_shortest_path(self, paths):
-        min_cycle_length = float('inf')
-        shortest_path = None
-        for path in paths:
-            cycle_length = sum(self.graph[path[i]][path[i+1]]['weight'] for i in range(len(path) - 1))
-            cycle_length += self.graph[path[-1]][path[0]]['weight']
-            if cycle_length < min_cycle_length:
-                min_cycle_length = cycle_length
-                shortest_path = path
-        return shortest_path, min_cycle_length
-
-
 class GraphApp:
     def __init__(self, root):
         self.root = root
@@ -159,9 +100,6 @@ class GraphApp:
 
         self.table.insert("", "end", values=("Итоговая стоимость пути:", "", f"{min_cycle_length:.2f}"))
 
-
-
-
     def clear_canvas(self):
         self.graph.clear()
         self.nodes.clear()
@@ -170,11 +108,67 @@ class GraphApp:
         self.table.delete(*self.table.get_children())
         self.node_count = 1
 
-    def distance(self, node1, node2):
-        x1, y1 = map(int, node1.strip("()").split(", "))
-        x2, y2 = map(int, node2.strip("()").split(", "))
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+class AntColony:
+    def __init__(self, graph, ants_count=10, evaporation_rate=0.1, pheromone_deposit=1, alpha=1, beta=2, iterations=100):
+        self.graph = graph
+        self.ants_count = ants_count
+        self.evaporation_rate = evaporation_rate
+        self.pheromone_deposit = pheromone_deposit
+        self.alpha = alpha
+        self.beta = beta
+        self.iterations = iterations
 
+    def find_shortest_path(self):
+        shortest_path = None
+        min_cycle_length = float('inf')
+
+        for _ in range(self.iterations):
+            paths = self.generate_ant_paths()
+            self.update_pheromones(paths)
+            current_shortest_path, current_cycle_length = self.get_shortest_path(paths)
+            if current_cycle_length < min_cycle_length:
+                shortest_path = current_shortest_path
+                min_cycle_length = current_cycle_length
+
+        return shortest_path, min_cycle_length
+
+    def generate_ant_paths(self):
+        paths = []
+        for _ in range(self.ants_count):
+            path = self.generate_ant_path()
+            paths.append(path)
+        return paths
+
+    def generate_ant_path(self):
+        nodes = list(self.graph.nodes)
+        random.shuffle(nodes)
+        return nodes
+
+    def update_pheromones(self, paths):
+        for path in paths:
+            cycle_length = sum(self.graph[path[i]][path[i+1]]['weight'] for i in range(len(path) - 1))
+            cycle_length += self.graph[path[-1]][path[0]]['weight']
+            for i in range(len(path) - 1):
+                node1 = str(path[i])
+                node2 = str(path[i+1])
+                self.graph[node1][node2]['pheromone'] = self.graph[node1][node2].get('pheromone', 0) + self.pheromone_deposit / cycle_length
+            node1 = str(path[-1])
+            node2 = str(path[0])
+            self.graph[node1][node2]['pheromone'] = self.graph[node1][node2].get('pheromone', 0) + self.pheromone_deposit / cycle_length
+
+        for edge in self.graph.edges:
+            self.graph.edges[edge]['pheromone'] *= (1 - self.evaporation_rate)
+
+    def get_shortest_path(self, paths):
+        min_cycle_length = float('inf')
+        shortest_path = None
+        for path in paths:
+            cycle_length = sum(self.graph[path[i]][path[i+1]]['weight'] for i in range(len(path) - 1))
+            cycle_length += self.graph[path[-1]][path[0]]['weight']
+            if cycle_length < min_cycle_length:
+                min_cycle_length = cycle_length
+                shortest_path = path
+        return shortest_path, min_cycle_length
 
 if __name__ == "__main__":
     root = tk.Tk()
